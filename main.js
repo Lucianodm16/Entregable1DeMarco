51bdc9fecbf3471ea7b7160672eaaa4c58015cab
Lucho De Marco <luchodemarco13@gmail.com>
2025-08-06 18:20:52 -0300

    tienda

--- main.js
+++ main.js
@@ -1,59 +1,43 @@
-import Producto from './Producto.js';
-import SuperMercado from './SuperMercado.js';
+const productos = [
+    {nombre : "Arroz", precio: 100, categoria: "Almacen", imagen: "https://super-uno.com.ar/wp-content/uploads/2024/05/7790070433114-jpg.webp"},
 
-const superProductos = [
-    {producto : "Arroz", precio: 100, cantidad: 15 ,categoria: "Almacen"},
-    {producto : "Fideos", precio: 80, cantidad: 10, categoria: "Almacen"},
-    {producto : "Pollo", precio: 50, cantidad: 40 , categoria: "Carnes"},
-    {producto : "Cerdo", precio: 60, cantidad: 30 ,  categoria: "Carnes"},
-    {producto : "Banana", precio: 30, cantidad: 20 , categoria: "Frutas"},
-    {producto : "Manzana", precio: 40, cantidad: 28 ,categoria: "Frutas"},
-    {producto : "Leche", precio: 20, cantidad: 56 , categoria: "Lacteos"},
-    {producto : "Queso", precio: 70, cantidad: 75 ,  categoria: "Lacteos"},
-    {producto : "Pan", precio: 25, cantidad: 63 , categoria: "Panaderia"},
-    {producto : "Galletitas", precio: 15, cantidad: 17 , categoria: "Panaderia"},
+    {nombre : "Fideos", precio: 80, categoria: "Almacen", imagen: "https://http2.mlstatic.com/D_NQ_NP_632227-MLA51761933428_092022-O.webp"},
+
+    {nombre : "Pollo", precio: 50, categoria: "Carnes", imagen: "https://www.cclataurina.com/wp-content/uploads/2024/05/pollo.webp"},
+
+    {nombre : "Cerdo", precio: 60, categoria: "Carnes", imagen: "https://frigorifico25demayo.com.ar/wp-content/uploads/2024/12/Huesito-Cerdo.webp"},
+
+    {nombre : "Banana", precio: 30 , categoria: "Frutas", imagen: "https://greenshop.ar/wp-content/uploads/2024/06/A.2.6-500GR.jpg"},
+
+    {nombre : "Manzana", precio: 40 ,categoria: "Frutas", imagen: "https://andreuprados.com/wp-content/uploads/2017/01/apple_0.jpg.webp"},
+
+    {nombre : "Pera", precio: 30 ,categoria: "Frutas", imagen: "https://frutaseloy.com/wp-content/uploads/2014/05/pera-agua-300x270.png.webp"},
+
+    {nombre : "Leche", precio: 20, categoria: "Lacteos", imagen: "https://polardistribuciones.com.ar/332-large_default/leche-larga-vida-descremada-1-lt--milkaut.webp"},
+
+    {nombre : "Queso", precio: 70,  categoria: "Lacteos", imagen: "https://http2.mlstatic.com/D_NQ_NP_798461-MLA70437426042_072023-O.webp"},
+
+    {nombre : "Pan", precio: 25 , categoria: "Panaderia", imagen: "https://www.webconsultas.com/sites/default/files/styles/wc_adaptive_image__small/public/media/0d/temas/pan_0.jpg.webp"},
+
+    {nombre : "Galletitas", precio: 15 , categoria: "Panaderia", imagen: "https://distribuidorafresh.com.ar/wp-content/uploads/2024/01/CHIPSDECHOCOLATE.webp"},
 ];
-const productosInstanciados = superProductos.map(item => {
-    return new Producto(item.producto, item.precio, item.cantidad, item.categoria);
+
+const carritoBoton = document.getElementById("card-button");
+const contenedor = document.querySelector(".contenedorBoxes");
+
+contenedor.innerHTML = "";
+
+productos.forEach((producto) => {
+    contenedor.innerHTML += `
+        <section class="productosContainer">
+            <img src="${producto.imagen}" 
+            alt="imagen de ${producto.nombre}">
+            <h2>${producto.nombre}</h2>
+            <p>Categoria: ${producto.categoria}</p>
+            <p>Precio: $${producto.precio}</p>
+            <button>Agregar al carrito</button>
+        </section>`;
 });
 
-const superMercado = new SuperMercado(productosInstanciados);
 
-function buscador(){  
-    let bandera = true;
-
-    while (bandera) {
-        let opcion = Number(prompt("Ingrese una opción:\n1. Buscar por nombre\n2. Buscar productos mayor o igual que\n3. Buscar por categoría\n4. Listar productos \n5. agregar Producto \n6. Actualizar precios con IVA"));
-        switch (opcion) {   
-            case 1:
-                superMercado.buscarPorNombre();
-                break;
-            case 2:
-                superMercado.buscarPorPrecio();
-                break;
-            case 3:
-                superMercado.buscarPorCategoria();
-                break;
-            case 4:
-                superMercado.listarProductos();
-                break;
-            case 5:
-                superMercado.agregarProducto();
-                break;
-            case 6:
-                superMercado.actualizarPrecioConIVA();
-                break;
-            default:
-                alert("Opción no válida, por favor intente de nuevo.");
-        }
-
-        let quiereSalir = confirm("¿Desea seguir buscando productos?");
-        if (!quiereSalir) {
-            alert("Gracias por usar el buscador. ¡Hasta luego!");
-            bandera = false; // Salir del bucle
-        }
-    }
-}
-
-
-buscador();
+renderizador();
